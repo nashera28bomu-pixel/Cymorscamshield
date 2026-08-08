56c9539908459a76101608f1204b4a429a77a56f
nashera28bomu-pixel <nashera28bomu@gmail.com>
2026-08-08 18:14:29 +0300

    Add contentHash field and index to ScamReport schema
    Added contentHash field to the schema and created an index for it.

--- models/ScamReport.js
+++ models/ScamReport.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 const reportSchema = new mongoose.Schema({
   reportedUrl: String,
   reportedBy: String,
+  contentHash: String,
 }, { timestamps: true });
+
+reportSchema.index({ contentHash: 1 });
 
 module.exports = mongoose.model('ScamReport', reportSchema);
